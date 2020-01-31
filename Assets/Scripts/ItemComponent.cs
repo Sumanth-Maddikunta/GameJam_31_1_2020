b4a715cf3933bc3a02c2daea1f84bc8c378174f0
SricharanYamsani <43514338+SricharanYamsani@users.noreply.github.com>
2020-02-01 00:36:45 +0530

    ItemComponent script added

---
+++ Assets/Scripts/ItemComponent.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemComponent : MonoBehaviour
+{
+    public EItem item = EItem.NONE;
+    public EItemState state = EItemState.NONE;
+    public int componentId = 0;
+
+    public Material silhouetteMaterial;
+    public Material normalMaterial;
+
+    Rigidbody rb;
+    MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        switch (state)
+        {
+            case EItemState.NONE:
+                break;
+            case EItemState.FIXED:
+                rb.isKinematic = true;
+                rb.useGravity = false;
+                meshRenderer.material = normalMaterial;
+
+                break;
+            case EItemState.SILHOUETTE:
+                rb.isKinematic = true;
+                rb.useGravity = false;
+                meshRenderer.material = silhouetteMaterial;
+
+                break;
+            case EItemState.BROKEN:
+                rb.isKinematic = false;
+                rb.useGravity = true;
+                meshRenderer.material = normalMaterial;
+
+                break;
+        }
+    }
+
+}
+
+public enum EItem
+{
+    NONE = -1,
+    CUBE,
+}
+
+public enum EItemState
+{
+    NONE = -1,
+    FIXED,
+    SILHOUETTE,
+    BROKEN,
+
+}
