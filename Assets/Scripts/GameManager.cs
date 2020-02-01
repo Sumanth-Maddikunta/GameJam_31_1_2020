bf085339a4627b6ea86565d8230b08e705bad4b3
Sumanth Maddikunta <sumanthmaddikunta@gmail.com>
2020-02-01 20:00:12 +0530

    Rotation UI and Game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        currentYRotation = currentObject.transform.localRotation.y;
+    }
+
+    public float rotationTime = 1f;
+    public GameObject currentObject;
+    float currentYRotation;
+    bool canRotate = true;
+    Tween rotationTween;
+
+    
+    public void GetCurrentGameObject()
+    {
+        currentObject = PlayerController.instance.control.gameObject;
+    }
+
+    public void RotateLeft()
+    {      
+        if (currentObject != null && (rotationTween.IsComplete() || rotationTween == null))
+        {
+            currentYRotation -= 90f;
+            Quaternion quaternion = Quaternion.Euler(0, currentYRotation, 0);
+            rotationTween =  currentObject.transform.DORotateQuaternion(quaternion, rotationTime).SetEase(Ease.InOutSine);
+        }
+    }
+
+    public void RotateRight()
+    {       
+        if (currentObject != null && (rotationTween.IsComplete() || rotationTween == null))
+        {
+            currentYRotation += 90f;
+            Quaternion quaternion = Quaternion.Euler(0, currentYRotation, 0);
+            rotationTween = currentObject.transform.DORotateQuaternion(quaternion, rotationTime).SetEase(Ease.InOutSine);
+        }
+    }
+}
