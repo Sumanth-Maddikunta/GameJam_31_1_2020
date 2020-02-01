60a9bc6e5b703ae6c4d625b572e794b88deb3209
Sumanth Maddikunta <sumanthmaddikunta@gmail.com>
2020-02-01 22:14:27 +0530

    Added question manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    AudioSource audioSource;
+    public List<AudioClass> audios = new List<AudioClass>();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if (instance != null)
+            {
+                Destroy(this);
+            }
+        }
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudioClip(string name)
+    {
+        for(int i=0;i<audios.Count;++i)
+        {
+            if(audios[i].audioName==name)
+            {
+                audioSource.clip = audios[i].audioClip;
+                audioSource.Play();
+                break;
+
+            }
+        }
+    }
+
+
+}
+
+[System.Serializable]
+public class AudioClass
+{
+    public string audioName;
+    public AudioClip audioClip;
+}
