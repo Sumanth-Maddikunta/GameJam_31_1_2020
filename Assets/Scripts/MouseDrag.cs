ba2fe88ac6ed9b0da3d63af93a4eb9e82562a275
Sumanth Maddikunta <sumanthmaddikunta@gmail.com>
2020-02-01 15:48:43 +0530

    DOTween and placement using mouse

---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MouseDrag : MonoBehaviour {
+
+    float distance = 10;
+
+    ObjectControl control;
+
+    private void Awake()
+    {
+        control = transform.GetComponentInParent<ObjectControl>();
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        for (int i = 0; i < control.brokenObjs.Count; ++i)
+        {
+            if (this.gameObject == control.brokenObjs[i])
+            {
+               
+                Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y,0f);
+                Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+                objectPos.z = control.oldPositions[i].transform.position.z;
+                transform.position = objectPos;
+            }            
+        }
+    }
+}
