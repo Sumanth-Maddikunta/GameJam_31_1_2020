d6e43f5f9491bd5fef1a30a07006868d66c9da74
Mohan Krishna <MohanKrishnaKanth@users.noreply.github.com>
2020-02-02 19:59:11 +0530

    Dialouge 2 levels

--- Assets/Scripts/Editor/DrawerEnumNamedArray.cs
+++ Assets/Scripts/Editor/DrawerEnumNamedArray.cs
@@ -3,20 +3,20 @@ using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
 
-[CustomPropertyDrawer(typeof(EnumNamedArrayAttribute))]
-public class DrawerEnumNamedArray : PropertyDrawer
-{
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        EnumNamedArrayAttribute enumNames = attribute as EnumNamedArrayAttribute;
-        //propertyPath returns something like component_hp_max.Array.data[4]
-        //so get the index from there
-        int index = System.Convert.ToInt32(property.propertyPath.Substring(property.propertyPath.IndexOf("[")).Replace("[", "").Replace("]", ""));
+//[CustomPropertyDrawer(typeof(EnumNamedArrayAttribute))]
+//public class DrawerEnumNamedArray : PropertyDrawer
+//{
+//    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+//    {
+//        EnumNamedArrayAttribute enumNames = attribute as EnumNamedArrayAttribute;
+//        //propertyPath returns something like component_hp_max.Array.data[4]
+//        //so get the index from there
+//        int index = System.Convert.ToInt32(property.propertyPath.Substring(property.propertyPath.IndexOf("[")).Replace("[", "").Replace("]", ""));
 
-        //change the label
-        label.text = enumNames.names[index];
+//        //change the label
+//        label.text = enumNames.names[index];
 
-        //draw field
-        EditorGUI.PropertyField(position, property, label, true);
-    }
-}
+//        //draw field
+//        EditorGUI.PropertyField(position, property, label, true);
+//    }
+//}
